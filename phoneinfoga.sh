39ee7b5c628681e149875b43cbf1e018fd100930
White Devil <71704807+NadarAthis@users.noreply.github.com>
2020-09-29 23:40:03 +0530

    Update phoneinfoga.sh

--- phoneinfoga.sh
+++ phoneinfoga.sh
@@ -7,20 +7,17 @@ clear
 cd $HOME
 sleep 2.0
 echo -e "\x1b[92m
-█▀█ █░█ █▀█ █▄░█ █▀▀ █ █▄░█ █▀▀ █▀█ █▀▀ ▄▀█
-█▀▀ █▀█ █▄█ █░▀█ ██▄ █ █░▀█ █▀░ █▄█ █▄█ █▀█"
+PHONENUMBERTRACKER
 echo -e "\x1b[91m
-█ █▄░█ █▀ ▀█▀ ▄▀█ █░░ █░░ █ █▄░█ █▀▀
-█ █░▀█ ▄█ ░█░ █▀█ █▄▄ █▄▄ █ █░▀█ █▄█ ▄ ▄ ▄
-
+INSTALLING
 "
 termux-setup-storage
 sleep 7.0
 rm -rf phoneinfoga.sh > /dev/null 2>&1
-echo -e '\033[1;91m[''\033[0m*''\033[1;91m]''\033[1;91m This Script will install PhoneInfoga Tool'
+echo -e '\033[1;91m[''\033[0m*''\033[1;91m]''\033[1;91m This Script will install PhoneNumberTracker Tool'
 echo
 sleep 2.0
-echo -e '\033[1;91m[''\033[0m*''\033[1;91m]''\033[1;91m Please Subscribe My YT Channel = Expert Anonymous'
+echo -e '\033[1;91m[''\033[0m*''\033[1;91m]''\033[1;91m Please Subscribe My YT Channel = Technical Patcher'
 echo
 sleep 2.0
 echo -e '\033[1;92m[''\033[0m*''\033[1;92m]''\033[1;92m Please wait...'
@@ -37,18 +34,18 @@ command -v python2 > /dev/null 2>&1 || { echo -e >&2 "\033[1;91mI require python
 
 sleep 2.0
 echo -e '\033[1;92m[''\033[0m*''\033[1;92m]''\033[1;92m Required Packages Installed.'
-rm -rf PhoneInfoga > /dev/null 2>&1
-rm -rf phoneinfoga > /dev/null 2>&1
+rm -rf PhoneNumberTracker > /dev/null 2>&1
+rm -rf PhoneNumberTracker > /dev/null 2>&1
 echo
 sleep 2.0
 echo -e '\033[1;92m[''\033[0m*''\033[1;92m]''\033[1;92m PhoneInfoga Installing...'
-git clone https://github.com/ExpertAnonymous/PhoneInfoga > /dev/null 2>&1 && cd PhoneInfoga > /dev/null 2>&1 && unzip PhoneInfoga > /dev/null 2>&1 && python3 -m pip install -r requirements.txt > /dev/null 2>&1 && pip2 install colorama > /dev/null 2>&1
-rm -rf PhoneInfoga.zip > /dev/null 2>&1
-rm -rf phoneinfoga.sh > /dev/null 2>&1
+git clone https://github.com/NadarAthis/PhoneNumberTracker > /dev/null 2>&1 && cd PhoneNumberTracker > /dev/null 2>&1 && unzip PhoneNumberTracker > /dev/null 2>&1 && python3 -m pip install -r requirements.txt > /dev/null 2>&1 && pip2 install colorama > /dev/null 2>&1
+rm -rf PhoneNumberTracker.zip > /dev/null 2>&1
+rm -rf PhoneNumberTracker.sh > /dev/null 2>&1
 chmod +x *
 sleep 2.0
 echo
-echo -e '\033[1;92m[''\033[0m*''\033[1;92m]''\033[1;92m PhoneInfoga Successfully Installed.'
+echo -e '\033[1;92m[''\033[0m*''\033[1;92m]''\033[1;92m PhoneNumberTracker Successfully Installed.'
 sleep 2.0
 echo
 printf $'\n\e[1;94m[\e[0m\e[1;77m*\e[0m\e[1;94m] Press Enter To Exit: \e[0m'
